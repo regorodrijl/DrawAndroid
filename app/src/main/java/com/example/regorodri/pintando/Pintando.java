794c86ab3a61327a03724c9f9e127db6ef98d8de
regorodrijl <regorodrijl@gmail.com>
2016-08-30 14:47:37 +0200

    cambios

--- app/src/main/java/com/example/regorodri/pintando/Pintando.java
+++ app/src/main/java/com/example/regorodri/pintando/Pintando.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.graphics.Bitmap;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
